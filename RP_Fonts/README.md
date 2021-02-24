76729ad0cddcd55470e2e4ead8060ca4276f0fcf
caderaspindrift <65486896+caderaspindrift@users.noreply.github.com>
2021-02-24 09:41:10 -0800

    Added RP_Fonts

---
+++ RP_Fonts/README.md
@@ -0,0 +1,5 @@
+# RP\_Fonts 
+
+These are some selected fonts for use with RP\_Tags and related addons, as
+well as a LibSharedMedia font manager.
+
---
+++ RP_Fonts/RP_Fonts.lua
@@ -0,0 +1,11 @@
+-- RP Tags 
+-- by Oraibi, Moon Guard (US) server
+-- ------------------------------------------------------------------------------
+-- This work is licensed under the Creative Commons Attribution 4.0 International
+-- (CC BY 4.0) license.
+
+local RPTAGS = RPTAGS;
+local addOnName, ns = ...;
+RPTAGS.queue:NewModule(addOnName, "rpClient");
+
+-- this doesn't do anything! Except it load and such if you have XRP.
