75c7745175d63f39d18210c49df0e1f99da7cc93
caderaspindrift <65486896+caderaspindrift@users.noreply.github.com>
2021-02-28 10:02:46 -0800

    Update fonts.lua

--- RP_Fonts/Fonts/fonts.lua
+++ RP_Fonts/Fonts/fonts.lua
@@ -8,34 +8,28 @@ ns.RP_Fonts.tmp = ns.RP_Fonts.tmp or {};
 
 
 local family = { 
-  Source = baseFontDir .. "Source_Code_Pro\\SourceCodePro-",
-  Share  = baseFontDir .. "ShareTechMono\\ShareTechMono-",
-  Syne   = baseFontDir .. "Syne_Mono\\SyneMono-",
-  MrsStD = baseFontDir .. "Mrs_Saint_Delafield\\MrsSaintDelafield-",
   Amara  = baseFontDir .. "Amarante\\Amarante-",
   Arima  = baseFontDir .. "Arima_Madurai\\ArimaMadurai-",
-  Barlow = baseFontDir .. "Barlow_Condensed\\BarlowCondensed-",
-  Bebas  = baseFontDir .. "Bebas_Neue\\BebasNeue-",
+  Brlow = baseFontDir .. "Barlow_Condensed\\BarlowCondensed-",
+  Bddebas  = baseFontDir .. "Bebas_Neue\\BebasNeue-",
   BigSho = baseFontDir .. "Big_Shoulders_Stencil_Display\\BigShouldersStencilDisplay-",
-  Bitter = baseFontDir .. "Bitter\\static\\Bitter-",
   Bree   = baseFontDir .. "Bree_Serif\\BreeSerif-",
-  Cedar  = baseFontDir .. "Cedarville_Cursive\\CedarvilleCursive-",
   DotG16 = baseFontDir .. "DotGothic16\\DotGothic16-",
   East   = baseFontDir .. "East_Sea_Dokdo\\EastSeaDokdo-",
   Flame  = baseFontDir .. "Flamenco\\Flamenco-",
-  Fonda  = baseFontDir .. "Fondamento\\Fontamento-",
   FontDS = baseFontDir .. "Fontdiner_Swanky\\FontdinerSwanky-",
   IMFell = baseFontDir .. "IM_Fell\\IMFell",
-  Krona  = baseFontDir .. "Krona_One\\KronaOne-",
-  Merri  = baseFontDir .. "Merriweather_Sans\\static\\MerriweatherSans-",
+  MrsStD = baseFontDir .. "Mrs_Saint_Delafield\\MrsSaintDelafield-",
   MtXmas = baseFontDir .. "Mountains_of_Christmas\\MountainsofChristmas-",
-  Oswald = baseFontDir .. "Oswald\\static\\Oswald-",
+  Oswald = baseFontDir .. "Oswald\\Oswald-",
   Poppi  = baseFontDir .. "Poppins\\Poppins-",
   Press  = baseFontDir .. "Press_Start_2P\\PressStart2P-",
   Prince = baseFontDir .. "Princess_Sofia\\PrincessSofia-",
   Reggae = baseFontDir .. "Reggae_One\\ReggaeOne-",
+  Share  = baseFontDir .. "ShareTechMono\\ShareTechMono-",
+  Source = baseFontDir .. "Source_Code_Pro\\SourceCodePro-",
+  Syne   = baseFontDir .. "Syne_Mono\\SyneMono-",
   Uncial = baseFontDir .. "Uncial_Antiqua\\UncialAntiqua-",
-
 };
 
 
@@ -71,7 +65,6 @@ local fontList=
   BigSho_Black  = { Load = false, Name = "Big Shoulders Stencil Display Black",      Fam = "BigSho", File = BLK                     },
   BigSho_Reg    = { Load = false, Name = "Big Shoulders Stencil Display",            Fam = "BigSho", File = REG                     },
   Bree_Reg      = { Load = false, Name = "Bree Serif",                               Fam = "Bree", File = REG                     },
-  Cedar_Reg     = { Load = false, Name = "Cedarville Cursive",                       Fam = "Cedar", File = REG                     },
   Flame_Light   = { Load = false, Name = "Flamenco Light",                           Fam = "Flame", File = LITE                    },
   Flame_Reg     = { Load = false, Name = "Flamenco",                                 Fam = "Flame", File = REG                     },
   FontDS_Reg    = { Load = false, Name = "Fontdiner Swanky",                         Fam = "FontDS", File = REG                     },
@@ -84,11 +77,9 @@ local fontList=
   MtXmas_Reg    = { Load = false, Name = "Mountains of Christmas",                   Fam = "MtXmas", File = REG                     },
   Oswald_Bold   = { Load = false, Name = "Oswald Bold",                              Fam = "Oswald", File = BOLD                    },
   Oswald_Reg    = { Load = false, Name = "Oswald",                                   Fam = "Oswald", File = REG                     },
-  Oswald_SemiBd = { Load = false, Name = "Oswald Semi-Bold",                         Fam = "Oswald", File = SEMIBD                 },
 
   Poppi_BlkIta  = { Load = false, Name = "Poppins Black Italic",                     Fam = "Poppi", File = BLK_ITA                 },
   Poppi_Bold    = { Load = false, Name = "Poppins Bold",                             Fam = "Poppi", File = BOLD                    },
-  Poppi_Italic  = { Load = false, Name = "Poppins Italic",                           Fam = "Poppi", File = ITA                     },
   Poppi_Reg     = { Load = false, Name = "Poppins",                                  Fam = "Poppi", File = REG                     },
 
   Press_Reg     = { Load = false, Name = "Press Start 2P",                           Fam = "Press", File = REG                     },
@@ -96,7 +87,6 @@ local fontList=
   Share_Reg     = { Load = true, Name = "Share Tech Mono",                          Fam = "Share", File = REG                     },
 
   Source_Bol    = { Load = false, Name = "Source Code Pro Bold",                     Fam = "Source", File = BOLD                    },
-  Source_BolIta = { Load = false, Name = "Source Code Pro Bold Italic",              Fam = "Source", File = BOLD_ITA                },
   Source_Ita    = { Load = false, Name = "Source Code Pro Italic",                   Fam = "Source", File = ITA                     },
   Source_Lig    = { Load = false, Name = "Source Code Pro Light",                    Fam = "Source", File = LITE                    },
   Source_Reg    = { Load = true, Name = "Source Code Pro",                          Fam = "Source", File = REG                     },
