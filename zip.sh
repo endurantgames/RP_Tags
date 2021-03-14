c16b7dbd7d166bdfc43e625fc01eab26047eefef
caderaspindrift <65486896+caderaspindrift@users.noreply.github.com>
2021-03-13 23:32:28 -0800

    New release 9.0.5.0 beta 3

--- zip.sh
+++ zip.sh
@@ -2,4 +2,5 @@
 
 # /usr/bin/zip -u RP_Tags.zip -r RP_Tags/ RP_Tags_ElvUI/ RP_Tags_MyRolePlay/ RP_Tags_totalRP3/ RP_UnitFrames/ -x \*old\* -x \*\.git\*
 /usr/bin/zip -u RP_Tags.zip -r RP_Tags/ RP_Tags_ElvUI/ RP_Tags_MyRolePlay/ RP_Tags_totalRP3/ RP_UnitFrames/ -x \*old\* -x \*\.git\*
+/usr/bin/zip -u RP_Fonts.zip -r RP_Fonts/ -x \*old\* -x \*\.git\*
 
