5e1236f6f304f9039cdfb0598f6779aa33ab470f
caderaspindrift <65486896+caderaspindrift@users.noreply.github.com>
2021-03-18 11:34:16 -0700

    Tweaked COMPACT layout

--- RP_UnitFrames/Layouts/compact.lua
+++ RP_UnitFrames/Layouts/compact.lua
@@ -8,40 +8,56 @@ function(self, event, ...)
   local layout = RPTAGS.utils.frames.RPUF_NewLayout("compact", "small");
 
   local function get_frame_dimensions(self)
-    local a = self:Public("Gap", 0.5) + 0;
-    local b = self:Public("PanelGet", "Height", "name") + 0;
-    local c = self:Public("PanelGet", "Height", "info") + 0;
-    local d = self:Public("ConfGet", "ICONWIDTH") + 0;
     return 
-      self:Public("Gap", 1.5) + self:Public("ConfGet", "ICONWIDTH") + self:Public("ConfGet", "INFOWIDTH"),
 
-      self:Public("Gap", 1) +
-      math.max( 
-        self:Public("Gap", 0.5) + self:Public("PanelGet", "Height", "name") + self:Public("PanelGet", "Height", "info"),
+      self:Public("Gap", 1.5) 
+      + self:Public("ConfGet", "ICONWIDTH") 
+      + self:Public("ConfGet", "RPUF_BORDER_INSETS") * 2
+      + self:Public("ConfGet", "INFOWIDTH"),
+
+      self:Public("Gap", 1) 
+      + self:Public("ConfGet", "RPUF_BORDER_INSETS") * 2
+      + math.max( 
+        self:Public("Gap", 0.5) 
+        + self:Public("PanelGet", "Height", "name") 
+        + self:Public("PanelGet", "Height", "info"),
         self:Public("ConfGet", "ICONWIDTH")
       )
   end;
 
   layout:Register_Panel_Method_Hash("GetPanelLeft",
-    { [ "icon1" ] = function(self) return self:Gap(0.5) end,
-      [ "name"  ] = function(self) return self:Gap(1) + self:ConfGet("ICONWIDTH") end,
+    { [ "icon1" ] = function(self) 
+                      return self:Gap(0.5) 
+                             + self:ConfGet("RPUF_BORDER_INSETS")
+                    end,
+      [ "name"  ] = function(self) 
+                      return self:Gap(1) 
+                             + self:ConfGet("ICONWIDTH") 
+                             + self:ConfGet("RPUF_BORDER_INSETS")
+                    end,
       [ "info"  ] = "name",
     });
 
   layout:Register_Panel_Method_Hash("GetPanelTop",
-    { [ "icon1" ] = function(self) return self:Gap(0.5) end,
+    { [ "icon1" ] = function(self) 
+                      return self:Gap(0.5) + self:ConfGet("RPUF_BORDER_INSETS")
+                    end,
       [ "name"  ] = "icon1",
-      [ "info"  ] = function(self) return self:Gap(0.5) + self:GetPanelHeight("name") end,
+      [ "info"  ] = function(self) 
+                      return self:Gap(1) 
+                             + self:GetPanelHeight("name") 
+                             + self:ConfGet("RPUF_BORDER_INSETS")
+                    end,
     });
 
   layout:Register_Panel_Method_Hash("GetPanelHeight",
     { [ "icon1" ] = function(self) return self:ConfGet("ICONWIDTH") end,
-      [ "name"  ] = function(self) return self:CalculateFontSize() + 4 end,
-      [ "info"  ] = function(self) return self:CalculateFontSize() + 2 end,
+      [ "name"  ] = function(self) return self:CalculateFontSize() end,
+      [ "info"  ] = function(self) return self:CalculateFontSize() end,
     });
 
   layout:Register_Panel_Method_Hash("GetPanelWidth",
-    { [ "icon1" ] = function(self) return self:ConfGet("ICONWIDTH") + 0.5 end,
+    { [ "icon1" ] = function(self) return self:ConfGet("ICONWIDTH") end,
       [ "name"  ] = "info",
       [ "info"  ] = function(self) return self:ConfGet("INFOWIDTH") end,
     });
